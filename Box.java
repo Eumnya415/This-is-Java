e1e7781aa6f5e63e29453b7b3ce9ffb4d10b08ad
CHA <145963611+Eumnya415@users.noreply.github.com>
2023-10-17 20:07:58 +0900

    제너릭 타입(1) : Box의 내용물을 비교하기 위해 타입 파라미터로 Object의 equals() 메소드 호출
    제너릭 타입(1)
    Box의 내용물을 비교하기 위해 타입 파라미터로 Object의 equals() 메소드 호출

---
+++ Box.java
@@ -0,0 +1,6 @@
+package org.study.run.exam1;
+
+public class Box<T>{  //제네릭 클래스 "Box"를 정의함. 이 클래스는 어떤 타입의 데이터도 저장할 수 있는 상자를 나타냄.
+    public T content;  //제네릭 타입 T를 사용하여 "content" 변수 선언. 변수는 상자에 저장되는 내용
+}
+
